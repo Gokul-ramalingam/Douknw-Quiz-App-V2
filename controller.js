b27785f9d9cf5168b156f1b14fa1867d403a6bb8
Gokul <gokulramalingam1@gmail.com>
2019-10-19 12:57:30 +0530

    Update in controller

--- controller.js
+++ controller.js
@@ -8,3 +8,10 @@ const app = express();
 
 const port = process.env.PORT || 9000;
 
+//MongoDB Configuration
+const db =  require('./setup/connection').url;
+
+mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
+                   .then('Connection with MongoDB established Successfully')
+                   .catch(err=> console.log('Error occurred while establishing connection with mongodb '+err));
+
