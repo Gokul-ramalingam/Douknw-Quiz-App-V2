9d8f5cc8022f4b890f7293c24375d6b0edd6f0dd
Gokul <gokulramalingam1@gmail.com>
2019-10-20 00:00:46 +0530

    Update in questions

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -8,15 +8,17 @@ const User = require('../../models/User')
 
 
 //type                        GET
-//@route                   /api/auth
+//@route                   /api/questions
 //@desc                    This route is to get the questions
 //@access                 Private
-router.get('/questions',passport.authenticate('jwt' , {session : false}),(req,res)=>{
- User.findOne({_id : req.user.id})
+router.get('/',(req,res)=>{
+ User.findOne({_id : req.username.id})
          .then(user => {
              if(!user)
-             return res.status(404).json({profilenotfount:'No Profile Found'});
+             return res.status(404).json({profilenotfount:req.username.id});
          res.json(user);
          })
          .catch(err => console.log('Something went wrong while searching for the user '+err));
 })
+
+module.exports = router;
