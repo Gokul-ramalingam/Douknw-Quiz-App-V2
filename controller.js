50afab51b7e9061e38543e16c289e0c86b623d58
Gokul <gokulramalingam1@gmail.com>
2019-10-19 15:39:29 +0530

    Update in controller

--- controller.js
+++ controller.js
@@ -8,10 +8,21 @@ const app = express();
 
 const port = process.env.PORT || 9000;
 
+//routes
+const auth = require('./routes/api/auth');
+
 //MongoDB Configuration
 const db =  require('./setup/connection').url;
 
 mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
-                   .then('Connection with MongoDB established Successfully')
+                   .then(()=> console.log("Connection with MongoDB established Successfully"))
                    .catch(err=> console.log('Error occurred while establishing connection with mongodb '+err));
+
+//Passport Middleware
+app.use(passport.initialize())
+
+//Main Routes
+app.use('/api/auth',auth)
+
+app.listen(port,()=>console.log(`App is running at port ${port}`));
 
