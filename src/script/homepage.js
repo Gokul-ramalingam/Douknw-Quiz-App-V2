c1b551e042ac6a0e3527575d61589b9cb8e98f95
Gokul <gokulramalingam1@gmail.com>
2019-10-21 03:32:31 +0530

    Updation in error handling

--- src/script/homepage.js
+++ src/script/homepage.js
@@ -1,6 +1,6 @@
 let init = () => {
     toggleSignin();
-    $('.usernameError,.emailError,.passwordError,.progress').hide();
+    $('.usernameError,.passwordError,.signinError,.progress,#spinup,#spinin').hide();
 }
 
 $( init );
@@ -24,7 +24,6 @@ let toggleSignin = () => {
 let passwordStrength = false;
 
 $("#signup").click(() => {
-
     let dataString = {}
     dataString.username = $('#username').val();
     dataString.email = $('#email').val();
@@ -33,6 +32,31 @@ $("#signup").click(() => {
     dataString.email === ''?$('#email').css('border','1px solid red'):null;
     dataString.password === ''?$('#password').css('border','1px solid red'):null;
     passwordStrength === false?$('#password').css('border','1px solid red')&&$('.passwordError').show():null;
+
+    if(dataString.username !=='' && dataString.email !=='' && passwordStrength !== false){
+        $('#signup').hide();
+        $('#spinup').show();
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify(dataString),
+            contentType: "application/json",
+            url: "http://localhost:9000/api/auth/register",
+            success: (data) => {
+                 localStorage.setItem('token',data.token.split(' ')[1]);
+                window.location.href='/src/html/section.html'
+            },
+            error: () => {
+                $('#signupForm')[0].reset();
+                $('#username').css('border','1px solid red');
+                $('.usernameError').show();
+                $('.bar').css('width','0%');
+            },
+            complete : () => {
+                $('#signup').show();
+                $('#spinup').hide();
+            }
+        });
+    }
 })
 
 
@@ -42,22 +66,57 @@ $("#signin").click(() => {
     dataString.password = $('#signinPassword').val();
     dataString.username === ''?$('#signinUsername').css('border','1px solid red'):null;
     dataString.password === ''?$('#signinPassword').css('border','1px solid red'):null;
+    if(dataString.username !== '' && dataString.password !== ''){
+
+        $('#signin').hide();
+        $('#spinin').show();
+        
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify(dataString),
+            contentType: "application/json",
+            url: "http://localhost:9000/api/auth/login",
+            success: (data) => {
+                localStorage.setItem('token',data.token.split(' ')[1]);
+                window.location.href='/src/html/section.html'
+            },
+            error: () => {
+                $('#signinPassword').val('');
+                $('#signinPassword').css('border','1px solid red');
+                $('.signinError').show();
+            },
+            complete: () => {
+                $('#signin').show();
+                $('#spinin').hide();
+            }
+        });
+    }
 })
 
 
 
 
 $("#username").click(() => {
-    $('#username').css('border','none')&&$('.usernameError').hide();
+    $('#username').css('border','none');
+    $('.usernameError').hide();
 })
 
 $("#email").click(() => {
-    $('#email').css('border','none')&&$('.emailError').hide();
+    $('#email').css('border','none');
 })
 
 $("#password").click(() => {
     $('.progress').show();
     $('#password').css('border','none')&&$('.passwordError').hide();
+})
+
+$("#signinUsername").click(() => {
+    $('#signinUsername').css('border','none');
+})
+
+$("#signinPassword").click(() => {
+    $('#signinPassword').css('border','none');
+    $('.signinError').hide();
 })
 
 
