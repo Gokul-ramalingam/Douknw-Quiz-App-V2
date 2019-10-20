41a184998d9853b4d9b3b59bfd94c9cdbc0929c8
Gokul <gokulramalingam1@gmail.com>
2019-10-20 17:28:25 +0530

    Complete folder structure

---
+++ src/script/question.js
@@ -0,0 +1,142 @@
+let timer = ()=>{
+    console.log("here")
+   let timer2 = "3:01";
+   let interval = setInterval(() =>{
+
+   let timer = timer2.split(':');
+   //by parsing integer, I avoid all extra string processing
+   let minutes = parseInt(timer[0], 10);
+   let seconds = parseInt(timer[1], 10);
+   --seconds;
+   minutes = (seconds < 0) ? --minutes : minutes;
+   if (minutes < 0) clearInterval(interval);
+   seconds = (seconds < 0) ? 59 : seconds;
+   seconds = (seconds < 10) ? '0' + seconds : seconds;
+   //minutes = (minutes < 10) ?  minutes : minutes;
+   $('#time').html(minutes + ':' + seconds);
+   timer2 = minutes + ':' + seconds;
+   if(minutes===0&&seconds==="00")      
+   location.href="section.html";               //If time reaches 0:00 taking back user to section page
+}, 1000);
+}
+
+let answerArray = [];
+let selectedOptions = [];
+let questions;
+
+//query return html or css or js from url 
+//Eg : douknw.com/questions?html
+let query = window.location.search.substring(1,); // ==> html
+
+if(query === 'html')
+{
+questions = JSON.parse( localStorage.getItem("htmlArray")); //Getting an array from local storage which is populated with 10 html questions
+localStorage.removeItem('htmlArray');//Removing it from local storage
+$('#sec').text(1);
+}
+else if(query === 'css')
+{
+    questions = JSON.parse(localStorage.getItem("cssArray"));//Getting an array from local storage which is populated with 10 html questions
+    localStorage.removeItem('cssArray');//Removing it from local storage
+    $('#sec').text(2);
+}
+else
+ {
+   questions = JSON.parse(localStorage.getItem("jsArray"));   //Getting an array from local storage which is populated with 10 html questions
+   localStorage.removeItem('jsArray');   //Removing it from local storage
+   $('#sec').text(3);
+ }
+
+
+$(document).ready(()=>{
+//To generate first question an automatic click is made to avoid conflicts
+$('#img-forward').click(()=>{
+ });
+ $('#img-forward').click();
+ timer();
+})
+
+
+let questionNumber = 0;
+$("#img-forward").click(()=>{
+        // This will protect next question does not exceed beyond the limit of 10 questions
+       
+       if(questionNumber < 10)
+       {
+        $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC'); //As soon as a new question gets loaded all the options background are given same background color
+        action('add'); // Here a function is being called and passed a parameter 'add' so that using that argument next question will get incremented
+        //The below if statement checks whether selectedOption array has any value in the particular index based on the questionnumber.  
+        if(selectedOptions[questionNumber-1]!=undefined) 
+       $("#option"+selectedOptions[questionNumber-1]).css('background-color','#78909C');//If that array have any option among 1,2,3&4 that particular option will get highlighted
+       }
+})
+
+
+$("#img-backward").click(()=>{
+
+    //Samething happens for this but in the opposite manner compared to img-forward.It check whether question doesn't exeed below 1;
+    if(questionNumber > 1)
+    {
+        $("#option1, #option2, #option3, #option4").css('background-color','#CFD8DC');
+        action('sub');
+       if(selectedOptions[questionNumber-1]!=undefined)
+        $("#option"+selectedOptions[questionNumber-1]).css('background-color','#78909C') ; 
+    }
+})
+let key;
+
+//In this function 'add' or 'sub' is passed as parameter using that argument function increments the question or decrements the question from array of 10 questions.
+let action =((option)=>{
+ option==='add'?questionNumber++:questionNumber--;
+if(questionNumber>=1 && questionNumber <= 10)
+{
+    key = Object.keys(questions[questionNumber-1])[1];
+    $("#num").text(questionNumber);
+    $("#question").text(questions[questionNumber-1][key].question);
+    $("#opt1").text(questions[questionNumber-1][key]["options"][0]);
+    $("#opt2").text(questions[questionNumber-1][key]["options"][1]);
+    $("#opt3").text(questions[questionNumber-1][key]["options"][2]);
+    $("#opt4").text(questions[questionNumber-1][key]["options"][3]);
+}
+})
+//When the user clicks on particular option that particular option gets highlighted and remaining options are changed to default color to avoid conflicts.
+ $("#option1").click(()=>{
+  selector(1);
+  $("#option2, #option3, #option4").css('background-color','#CFD8DC');
+ })
+
+ $("#option2").click(()=>{
+    selector(2);
+    $("#option1, #option3, #option4").css('background-color','#CFD8DC');
+   })
+
+   $("#option3").click(()=>{
+    selector(3);
+    $("#option1, #option2, #option4").css('background-color','#CFD8DC');
+   })
+
+   $("#option4").click(()=>{
+    selector(4);
+    $("#option1, #option2, #option3").css('background-color','#CFD8DC');
+   })
+//Selector stores the question number along with answer which was answer by user in local storage in form of array of objects.
+let selector = ((option)=>{
+    answerArray[questionNumber-1] ={key:key,answer : $("#opt"+option).text()};
+    selectedOptions[questionNumber-1]=option;
+    $("#option"+option).css('background-color','#78909C');
+})
+
+
+   $("#submitButton").click(()=>{
+       localStorage.setItem(query+"Answers",JSON.stringify(answerArray));
+       location.href = 'result.html'
+   })
+
+   $("#sectionButton").click(()=>{
+       localStorage.setItem(query+"Answers",JSON.stringify(answerArray));
+       if(localStorage.hasOwnProperty('htm') && localStorage.hasOwnProperty('cs')&&localStorage.hasOwnProperty('js'))
+       location.href = 'result.html';
+       else
+       location.href = 'section.html';
+   })
+ 
---
+++ src/script/section.js
@@ -0,0 +1,82 @@
+let htmlAr = [];
+let cssAr = [];
+let jsAr = [];
+
+//Ajax request is made to receive all the questions from mongodb database using node api.
+$(document).ready(function(){
+   localStorage.setItem('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkYWMzYzU4Y2JlNDU5MGY5ODI4YzFhNCIsInVzZXJuYW1lIjoiR29rdWwiLCJlbWFpbCI6Imdva3VscmFtYWxpbmdhbUBnbWFpbC5jb20iLCJpYXQiOjE1NzE1Njg3NDIsImV4cCI6MTU3MTU3OTU0Mn0.-_QaU-7jsQZIzgHiOOXyjwSECQSgjeU-7UAecFOV49I');
+    $.ajax({
+        url:'http://localhost:9000/api/questions',
+        type: 'GET',
+        headers: {
+         "Content-Type":"application/x-www-form-urlencoded",
+         "token":localStorage.getItem('token')
+     },
+        success:function(data){
+           //Filtering of question from a pool of multiple type questions
+            let htmlQuestions = data.filter(dt=>{
+                return dt[Object.getOwnPropertyNames(dt)[1]]["type"] === 'html';
+             })
+             randomPicker(htmlQuestions,htmlAr);//Filtered question of particular type and a empty array for that type is passed as parameter
+             count1 = -1;
+             let cssQuestions = data.filter(dt=>{
+                return dt[Object.getOwnPropertyNames(dt)[1]]["type"] === 'css';
+             })
+             randomPicker(cssQuestions,cssAr);//Filtered question of particular type and a empty array for that type is passed as parameter
+            count1 = -1;
+             let jsQuestions = data.filter(dt=>{
+                return dt[Object.getOwnPropertyNames(dt)[1]]["type"] === 'js';
+             })
+             randomPicker(jsQuestions,jsAr);//Filtered question of particular type and a empty array for that type is passed as parameter
+        }
+    })
+ 
+  });
+//If the localstorage has the particular item then opacity of that section is reduced to indicate it is not clickable.
+if(localStorage.getItem("htm")==="yes")
+    $("#htm").css("opacity",0.5);
+ 
+ if(localStorage.getItem("cs")==="yes")
+    $("#cs").css("opacity",0.5);
+
+ if(localStorage.getItem("js")==="yes")
+    $("#jst").css("opacity",0.5);
+
+    //Only if the particular section is being clicked for first time then user gets navigated to that page
+    $("#htm").click(()=>{
+      if(!localStorage.hasOwnProperty("htm"))
+      {
+      localStorage.setItem("htm","yes");
+      localStorage.setItem("htmlArray",JSON.stringify(htmlAr));
+     location.href='questions.html?html';
+      }
+   })
+  
+  $("#cs").click(()=>{
+   if(!localStorage.hasOwnProperty("cs"))
+   {
+     localStorage.setItem("cs","yes");
+     localStorage.setItem("cssArray",JSON.stringify(cssAr));
+     location.href='questions.html?css';
+   }
+  })
+  
+  
+  $("#jst").click(function(){
+   if(!localStorage.hasOwnProperty("js"))
+   {
+     localStorage.setItem("js","yes");
+     localStorage.setItem("jsArray",JSON.stringify(jsAr));
+     location.href='questions.html?js';
+   }
+  })
+
+
+//This function generate ten random question from a pool of  multiple questions which are all of different type.
+let randomPicker=(json,ar)=>{
+    for(let i = 0;i < 10;i++){
+    let randIndex = Math.floor(Math.random()*json.length)
+    ar.push(json[randIndex])
+    json.splice(randIndex,1);
+    }
+}
